144954832623e0572463d769551d5ebe250792e6
Razvan Luta <razvan.luta@gmail.com>
2020-11-12 15:23:50 -0800

    WIP writing the demo code for processing the mesh in Blender.

--- BlenderScripts/process_mesh.py
+++ BlenderScripts/process_mesh.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import bpy
 
@@ -22,5 +23,53 @@ def get_args():
 	return parsed_script_args
 
 
-args = get_args()
-print("The passed asset path is: " + args.path)
+def setup_and_run_mesh_process():
+	"""
+	:return:
+	"""
+	args = get_args()
+	source_asset_path = args.path
+	process_mesh(source_asset_path)
+	
+
+def process_mesh(asset_path):
+	"""
+	:param asset_path:
+	:return:
+	"""
+	processed_mesh_suffix = "_processed"
+	fbx_extension = ".fbx"
+	
+	asset_name = os.path.splitext(os.path.basename(asset_path))[0]
+	source_asset_directory = os.path.dirname(asset_path)
+	
+	# Determine new naming and paths for the processed mesh
+	export_asset_name = asset_name + processed_mesh_suffix
+	export_asset_path = os.path.join(source_asset_directory, export_asset_name + fbx_extension)
+	
+	print("The source asset path is: " + asset_path)
+	print("The source asset name is: " + asset_name)
+	print("The source directory path is: " + source_asset_directory)
+	
+	# Clear the Blender scene
+	bpy.ops.object.select_all(action="SELECT")
+	bpy.ops.object.delete()
+	
+	# Import the asset in the Blender scene
+	bpy.ops.import_scene.fbx(filepath=asset_path)
+	imported_assets = bpy.context.selected_objects
+	print("Imported assets: " + str(imported_assets))
+	
+	# Process the asset
+	# In this sample, I'm smoothing the asset and exporting the new mesh right next to the old one.
+	# Add your custom processing here.
+	print("Processing")
+	
+	# Export the asset from Blender back to Unity, next to the original asset
+	bpy.ops.export_scene.fbx(
+		filepath=export_asset_path,
+		use_selection=True)
+	
+	
+# Triggering the mesh process
+setup_and_run_mesh_process()

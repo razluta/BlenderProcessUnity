0c50506542c6b3110eacb36a80e7a105ab26c607
Razvan Luta <razvan.luta@gmail.com>
2020-11-12 10:47:00 -0800

    Wip running .bat from Unity.

---
+++ BlenderScripts/run_mesh_process.bat
@@ -0,0 +1,2 @@
+echo "Raz"
+pause
--- UnityProject/Assets/Scripts/Editor/MeshBlenderProcess.cs
+++ UnityProject/Assets/Scripts/Editor/MeshBlenderProcess.cs
@@ -1,22 +1,49 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Diagnostics;
 using UnityEditor;
+using UnityEngine;
 
 namespace Editor
 {
     public class MeshBlenderProcess : UnityEditor.Editor
     {
+        private const string CommandPromptPath = "C:\\Windows\\system32\\CMD.exe";
+        private const string BlenderScriptPartition = "/D";
+        private const string BlenderScriptPath = 
+            "D:\\example-repos\\BlenderProcessUnity\\BlenderScripts\\run_mesh_process.bat";
         private const string MenuItemBlenderToolsRunMeshProcess = "Assets/Blender Tools/Run Mesh Process";
 
         // Showing a menu item for the process
         [MenuItem(MenuItemBlenderToolsRunMeshProcess)]
         private static void RunMeshProcess()
         {
-            Debug.Log("Clicked menu!");
+            try 
+            {
+                var myProcess = new Process
+                {
+                    StartInfo =
+                    {
+                        //WindowStyle = ProcessWindowStyle.Hidden,
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                        FileName = CommandPromptPath,
+                        Arguments = BlenderScriptPartition + BlenderScriptPath
+                    },
+                    EnableRaisingEvents = true
+                };
+                myProcess.Start();
+                myProcess.WaitForExit();
+                UnityEngine.Debug.Log("Successfully ran");
+            }
+            catch (Exception exception)
+            {
+                UnityEngine.Debug.Log(exception);
+            }
         }
         
         // Validating the menu item can be executed
         [MenuItem(MenuItemBlenderToolsRunMeshProcess, true)]
-        static bool ValidateRunMeshProcess()
+        private static bool ValidateRunMeshProcess()
         {
             return true;
         }

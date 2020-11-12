e2b413707dbfa5b0e8678e93150702ec2a59d398
Razvan Luta <razvan.luta@gmail.com>
2020-11-12 02:03:06 -0800

    Started a MenuItem example.

---
+++ UnityProject/Assets/Scripts/Editor/MeshBlenderProcess.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Editor
+{
+    public class MeshBlenderProcess : UnityEditor.Editor
+    {
+        private const string MenuItemBlenderToolsRunMeshProcess = "Assets/Blender Tools/Run Mesh Process";
+
+        // Showing a menu item for the process
+        [MenuItem(MenuItemBlenderToolsRunMeshProcess)]
+        private static void RunMeshProcess()
+        {
+            Debug.Log("Clicked menu!");
+        }
+        
+        // Validating the menu item can be executed
+        [MenuItem(MenuItemBlenderToolsRunMeshProcess, true)]
+        static bool ValidateRunMeshProcess()
+        {
+            return true;
+        }
+    }
+
+}
